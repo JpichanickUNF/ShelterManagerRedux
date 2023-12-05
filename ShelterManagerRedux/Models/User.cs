c0f034360e54281ca35cdbe187a1d9039795b860
Brandon Phoumyyoth <n01489590@unf.edu>
2023-12-05 18:23:47 -0500

    Added first and last name to shelter manager account

--- ShelterManagerRedux/Models/User.cs
+++ ShelterManagerRedux/Models/User.cs
@@ -5,6 +5,14 @@ public class User
     public int Id { get; set; }
 
     [Required]
+    [StringLength(20)]
+    public string firstname { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string lastname { get; set; }
+
+    [Required]
     [StringLength(50)]
     public string Username { get; set; }
 
