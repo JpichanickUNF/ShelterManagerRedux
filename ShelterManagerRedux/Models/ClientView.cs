ba20e4ce0bf68b5198f602b779f837565574e107
Sytsn <jwclaus12@gmail.com>
2023-12-10 23:09:29 -0500

    made ClientView Model connected to database

---
+++ ShelterManagerRedux/Models/ClientView.cs
@@ -0,0 +1,16 @@
+﻿namespace ShelterManagerRedux.Models
+{
+    public class ClientView
+    {
+        public int Shelter_Location_ID { get; set; }
+
+        public string Shelter_Location_Description { get; set; }
+
+
+        public int AvailableSpace { get; set; }
+
+        public string TotalSpace { get; set; }
+
+
+    }
+}
