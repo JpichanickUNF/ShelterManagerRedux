b7a8ce42f6bb2d4847c243d10122dd7118a58b21
JpichanickUNF <103592946+JpichanickUNF@users.noreply.github.com>
2024-02-03 15:31:12 -0500

    Fixed some formatting, improved database, removed footer

--- ShelterManagerRedux/Models/ShelterLocation.cs
+++ ShelterManagerRedux/Models/ShelterLocation.cs
@@ -8,5 +8,10 @@ namespace ShelterManagerRedux.Models
         [Key]
         public int Shelter_Location_ID { get; set; }
         public string Shelter_Location_Description { get; set; }
+
+        public int Shelter_Location_Available_Room { get; set; }
+
+        public int Shelter_Location_Total_Room { get; set; }
+
     }
 }
